c2887219fff803014fe06b7398c6f14d9b5dda16
ramzYka <ybloko2003god@gmail.com>
2025-09-17 14:21:51 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <input type="text" v-model="name" placeholder="Name">
+    <input type="password" v-model="passwrd" placeholder="Password">
+    <input type="email" v-model="email" placeholder="Email">
+    <p className="error">{{ error }}</p>
+    
+    <button @click="sendData()">To send</button>
+
+    <div v-if="users.length == 0">Нет зареганых пользователей</div>
+    
+    <div v-for="el,index in users" :key="index">
+      {{ el.name }}
+      {{ el.password }}
+      {{ el.mail }}
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import User from "./components/"
+
+  export default {
+    data() {
+      return {
+        name: '',
+        passwrd: '',
+        email: '',
+        error: '',
+        users: []
+      }
+    },
+
+    methods: {
+      sendData() {
+
+        if(this.name == '') {
+          this.error = "Вы ничего не ввели"
+          return;
+        } else if(this.passwrd == '') {
+          this.error = "Вы ничего не ввели"
+          return;
+        } else if(this.email == '') {
+          this.error = "Вы ничего не ввели"
+          return
+        }
+
+        this.error = ''
+
+        this.users.push({
+          name: this.name,
+          password: this.passwrd,
+          mail: this.email,
+        })
+      }
+    }
   }
-}
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+  h3 {
+    font-weight: lighter;
+  }
+
+  p {
+    color: red
+  }
 </style>
